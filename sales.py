22e56fb0d0104119321ef455a896a5fea62f80e7
Ranga Prasath S R <rangaprasath.sr.2024.cse@rajalakshmi.edu.in>
2026-05-16 10:01:08 +0530

    Fix syntax errors: change Print to print and fix missing quote

--- sales.py
+++ sales.py
@@ -1,17 +1,17 @@
-Print("Welcome to The Sales Tracking System")
-s=input("Enter Your name")
+print("Welcome to The Sales Tracking System")
+s = input("Enter Your name: ")
 print("Menu")
 print("________")
 print("1.Add Sales")
-print("2.See History ")
+print("2.See History")
 print("3.Logout")
-d=int(input("Enter your choice:"))
-sales=[]
-if(d==1):
-  n=int(input("Enter your sales record"))
-  sales.append(n)
-elif(d==2):
-  print("This is your history")
-  print(sales)
+d = int(input("Enter your choice: "))
+sales = []
+if(d == 1):
+    n = int(input("Enter your sales record: "))
+    sales.append(n)
+elif(d == 2):
+    print("This is your history")
+    print(sales)
 else:
-  print("Logging out Goodbye....)
+    print("Logging out Goodbye....")
